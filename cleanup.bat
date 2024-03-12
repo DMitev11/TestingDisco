59b1ae2f8aa6edb2bae92ab48d5639507ba31db3
drgmitev <drgmitev@gmail.com>
2024-03-12 23:28:39 +0200

    clean up

---
+++ cleanup.bat
@@ -0,0 +1,11 @@
+del /s /q /f *.pdb
+del /s /q /f *.ilk
+del /s /q /f *.exp
+del /s /q /f *.lib
+del /s /q /f *.obj
+del /s /q /f *.pch
+del /s /q /f *.idb
+del /s /q /f *.pdb
+
+rem Clean up derived data cache
+rd /s /q "DerivedDataCache"
